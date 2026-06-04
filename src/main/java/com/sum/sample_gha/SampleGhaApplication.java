f9d39de4ca20de3d4c5b4af05122d45c711753e2
Sumanth GM <214g1a32a4@srit.ac.in>
2026-06-04 15:49:41 +0530

    Initial commit

---
+++ src/main/java/com/sum/sample_gha/SampleGhaApplication.java
@@ -0,0 +1,13 @@
+package com.sum.sample_gha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sum/sample_gha/SampleGhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.sample_gha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
